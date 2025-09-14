3d05c9a302dbcd6554a296c3543d12fdf82454e0
lien0219 <lien0001@163.com>
2025-09-14 19:43:16 +0800

    fix:修复注册账号id自增问题

--- internal/model/base.go
+++ internal/model/base.go
@@ -9,7 +9,7 @@ import (
 // BaseModel is a replacement for gorm.Model with Swagger documentation
 // swagger:model
 type BaseModel struct {
-	ID        uint           `gorm:"primaryKey" json:"id"`
+	ID        uint           `gorm:"primaryKey;autoIncrement" json:"id"`
 	CreatedAt time.Time      `json:"createdAt"`
 	UpdatedAt time.Time      `json:"updatedAt"`
 	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
